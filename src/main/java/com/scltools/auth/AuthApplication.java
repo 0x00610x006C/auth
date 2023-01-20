96b2f759de728b463c29453dfb9f3e288c1f8eb4
0x00610x006C <i.m.allesssandro@gmail.com>
2023-01-20 13:43:38 +0300

    create repository for user

--- src/main/java/com/scltools/auth/AuthApplication.java
+++ src/main/java/com/scltools/auth/AuthApplication.java
@@ -2,11 +2,14 @@ package com.scltools.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
+@EnableJdbcRepositories
 public class AuthApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(AuthApplication.class, args);
 	}
 
